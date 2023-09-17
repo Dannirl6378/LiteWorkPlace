2ec49962bdcc7a3723d5a9f27daae4d618ebc689
Daniel <allprogsandprojects.@gmail.com>
2023-09-17 20:52:04 +0200

    feat: stále stylovani

--- src/SingUpIn/Sing.tsx
+++ src/SingUpIn/Sing.tsx
@@ -7,8 +7,9 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from '@mui/icons-material/Clear';
 import "./Sing.css";
-import shadows from "@mui/material/styles/shadows";
+
 
 interface SingProps {
   setConfirmed: (confirmed: boolean) => void;
@@ -16,11 +17,16 @@ interface SingProps {
 
 export default function Sing(props: SingProps) {
   const [action, setAction] = useState("Sing Up");
+  const [pwd,setPwd]=useState("");
+  const [matchPwd,setMatchPwd]=useState("");
+  const [user,setUser]=useState("");
+  const [email,setEmail]=useState("");
   return (
+    <div className="background">
     <div className="form">
       <Box
         sx={{
-          maxWidth: "20vw",
+          maxWidth: "fit-content",
           maxHeight: "fit-content",
           marginTop: "15%",
           marginLeft: "35%",
@@ -32,49 +38,51 @@ export default function Sing(props: SingProps) {
       >
         {action === "Sing Up" ? <h1>Sing Up</h1> : <h1>Sing In</h1>}
 
-
-        <CheckIcon />
-        <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+        <Box sx={{ display: "flex", alignItems: "flex-end",flexDirection:"row", }}>
           <Person2OutlinedIcon
             sx={{ color: "action.active", mr: 1, my: 0.5 }}
           />
-          <TextField id="input-with-sx" label="Name" variant="standard" />
+          <TextField id="inputName" label="Name" variant="standard" onChange={(e) => setUser(e.target.value)} />
+          <CheckIcon color="success" sx={{ display: user ? "" : "none" }}/>
         </Box>
 
-
-        <CheckIcon />
-        <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+        <Box sx={{ display: "flex", alignItems: "flex-end",flexDirection:"row", }}>
           <EmailOutlinedIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-          <TextField id="input-with-sx" label="Email" variant="standard" />
+          <TextField id="inputEmail" label="Email" variant="standard" onChange={(e) => setEmail(e.target.value)}/>
+          <CheckIcon color="success" sx={{ display: email ? "" : "none" }}/>
         </Box>
 
-
-        <CheckIcon />
         <Box
           sx={{
             display: "flex",
             alignItems: "flex-end",
             padding: "0em 0em 1em",
+            flexDirection:"row",
           }}
         >
           <LockOutlinedIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-          <TextField id="input-with-sx" label="Password" variant="standard" />
+          <TextField id="inputPassword" type="password" label="Password" value={pwd} variant="standard" onChange={(e) => setPwd(e.target.value)}/>
+          <CheckIcon color="success" sx={{ display: pwd ? "" : "none" }} />
         </Box>
         {action === "Sing Up" ? 
         <>
-        <CheckIcon />
         <Box
             sx={{
               display: "flex",
               alignItems: "flex-end",
-              padding: "0em 0em 1em",
+              padding: "0em 0em 1em ",
+              flexDirection:"row",
             }}
           >
             <LockOutlinedIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
             <TextField
-              id="input-with-sx"
-              label="Confirm Password"
-              variant="standard" />
+              id="inputConfirmPassword"
+              label="ConfirmPassword"
+              variant="standard"
+              type="password"
+              onChange={(e) => setMatchPwd(e.target.value)} 
+              />
+              {pwd===matchPwd?<CheckIcon color="success" sx={{ display: matchPwd ? "" : "none" }}/>:<ClearIcon color="error"/>}
           </Box>
           </>
           :""}
@@ -93,6 +101,7 @@ export default function Sing(props: SingProps) {
             variant="contained"
             onClick={() => {
               setAction("Sing In");
+              setMatchPwd("");
             }}
           >
             Sing In
@@ -107,6 +116,7 @@ export default function Sing(props: SingProps) {
           </Button>
         </Stack>
       </Box>
+    </div>
     </div>
   );
 }
