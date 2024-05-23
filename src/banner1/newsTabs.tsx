1caaf399d31a42198821cc868b7c64d84afd6b65
Dannirl6378 <Adner147@gmail.com>
2024-05-23 20:31:03 +0200

    feat: trocbu csss formatovani news

--- src/banner1/newsTabs.tsx
+++ src/banner1/newsTabs.tsx
@@ -13,19 +13,19 @@ export default function NewsTabs() {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ flexGrow: 1, display: 'flex', height: "90",paddingRight:"20px",overflow: 'auto' }}>
       <Tabs
+        orientation="vertical"
         value={value}
         onChange={handleChange}
         textColor="secondary"
-        indicatorColor="secondary"
-        aria-label="secondary tabs example"
+        sx={{ display: 'flex', justifyContent: 'flex-start',marginRight:"5px",paddingRight:"3px"}}
       >
         <Tab sx={{ fontSize: 15, fontWeight: 'bold'}}  value="one" label="News1" />
         <Tab sx={{ fontSize: 15, fontWeight: 'bold'}} value="two" label="NewsApi" />
         <Tab sx={{ fontSize: 15, fontWeight: 'bold'}}  value="three" label="News3" />
       </Tabs>
-      <Box sx={{ mt: 1 }}>
+      <Box sx={{ mt: 0 }}>
         {value === 'one' && <News1 />} 
         {value === 'two' && <News2 />} 
         </Box>
