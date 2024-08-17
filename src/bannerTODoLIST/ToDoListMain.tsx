50c43a635db34afa81717f9d76eda4e95df6b181
Dannirl6378 <127243165+Dannirl6378@users.noreply.github.com>
2024-08-17 23:05:16 +0200

    feat: zapomel sem udelat nove vlakno na todoList hotvo 1

--- src/bannerTODoLIST/ToDoListMain.tsx
+++ src/bannerTODoLIST/ToDoListMain.tsx
@@ -1,37 +1,92 @@
-import React, {  FunctionComponent, useState } from 'react'
-import { Button,Menu,MenuItem,TextField,IconButton,Box } from '@mui/material';
-import DeleteIcon from "@mui/icons-material/Delete"
-interface ToDoListProps {
-    
-}
- 
-const ToDoList: FunctionComponent<ToDoListProps> = () => {
-    const [showList,setShowList]=useState(false);
+import React, { FunctionComponent, useState, MouseEvent } from "react";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  TextField,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
+export default function ToDoList() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [items, setItems] = useState<string[]>([]);
+  const [newItem, setNewItem] = useState<string>("");
 
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleAddItem = () => {
+    if (newItem.trim() !== "") {
+      setItems((prevItems) => [...prevItems, newItem]);
+      setNewItem("");
+    }
+  };
+  const handleDeleteItems = (index: number) => {
+    setItems((prevItems) => prevItems.filter((_, i) => i !== index));
+  };
 
-    return ( <div>
-        <Button>
-            Open ToDo List
-        </Button>
-        <Menu open={showList}>
-        <Box>
-            <TextField>
-
-            </TextField>
-            <MenuItem>
-
-            </MenuItem>
-            <Button>Add</Button>
-            <Box>
-                <IconButton>
-                    <DeleteIcon/>
+  return (
+    <div>
+      <Button
+        aria-controls="ToDoList"
+        aria-haspopup="tree"
+        onClick={handleClick}
+      >
+        Open ToDo List
+      </Button>
+      <Menu
+        id="ToDo List"
+        open={Boolean(anchorEl)}
+        keepMounted
+        onClose={handleClose}
+        sx={{
+          style: {
+            maxHeight: 300,
+            width: "20ch",
+          },
+        }}
+      >
+        <MenuItem>
+          <TextField
+            label="Add Text"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleAddItem();
+                e.preventDefault();
+              }
+            }}
+            fullWidth
+          />
+        </MenuItem>
+        <List dense>
+          {items.map((item, index) => (
+            <ListItem
+              key={index}
+              secondaryAction={
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() => handleDeleteItems(index)}
+                >
+                  <DeleteIcon />
                 </IconButton>
-            </Box>
+              }
+            >
+              <ListItemText primary={item} />
+            </ListItem>
+          ))}
+        </List>
+      </Menu>
+    </div>
+  );
+};
 
-        </Box>
-        </Menu>
-    </div> );
-}
- 
-export default ToDoList;
