cb4e17de5dcca1086d22fafd05340adc4f1825a8
Dannirl6378 <127243165+Dannirl6378@users.noreply.github.com>
2025-01-28 22:28:20 +0100

    feat: oprava registrace

--- src/SingUpIn/Register.tsx
+++ src/SingUpIn/Register.tsx
@@ -2,7 +2,11 @@ import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export async function handleRegistration(user: string, email: string, pwd: string) {
+export async function handleRegistration(
+  user: string,
+  email: string,
+  pwd: string,
+) {
   const userData = {
     name: user,
     email: email,
@@ -10,7 +14,10 @@ export async function handleRegistration(user: string, email: string, pwd: strin
   };
 
   try {
-    const response = await axios.post("http://localhost:3001/api/register", userData);
+    const response = await axios.post(
+      "https://muj-backend-jxbr.onrender.com/api/register",
+      userData,
+    );
 
     // Zobrazíme notifikaci o úspěchu
     toast.success("Registrace úspěšná, můžete se přihlásit!");
@@ -19,8 +26,13 @@ export async function handleRegistration(user: string, email: string, pwd: strin
     // Při chybě zobrazíme notifikaci s chybovým hlášením
     if (axios.isAxiosError(error)) {
       // Pokud se jedná o Axios chybu
-      toast.error(`Chyba při registraci: ${error.response?.data?.error || error.message}`);
-      console.error("Registration error:", error.response?.data?.error || error.message);
+      toast.error(
+        `Chyba při registraci: ${error.response?.data?.error || error.message}`,
+      );
+      console.error(
+        "Registration error:",
+        error.response?.data?.error || error.message,
+      );
     } else if (error instanceof Error) {
       // Při jiných typech chyb
       toast.error(`Neočekávaná chyba při registraci: ${error.message}`);
