b68ff9b1da9893e2c71196455d2a12ae76ee156f
Dannirl6378 <127243165+Dannirl6378@users.noreply.github.com>
2025-03-02 19:43:58 +0100

    feat: uprava alarmu pro obily vzhled

--- src/1stBanner/ReminderClock/clockRemind.tsx
+++ src/1stBanner/ReminderClock/clockRemind.tsx
@@ -3,18 +3,16 @@ import { useState, useEffect } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import AlarmIcon from "@mui/icons-material/Alarm";
 import dayjs, { Dayjs } from "dayjs";
-
-interface TimeObject {
-  [x: string]: string | number | Date;
-  hours: number;
-  minutes: number;
-}
+import { IconButton } from "@mui/material";
 
 const AlarmClock: React.FC = () => {
   const [selectedTime, setSelectedTime] = useState<Dayjs | null>(null);
   const [currentTime, setCurrentTime] = useState<Dayjs>(dayjs());
   const [alarmTime, setAlarmTime] = useState<Dayjs | null>(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [open, setOpen] = useState(false); // Stát pro otevření TimePickeru
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -42,20 +40,48 @@ const AlarmClock: React.FC = () => {
     if (newTime) {
       setAlarmTime(newTime);
     }
+    setOpen(false); // Zavře TimePicker po výběru času
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <TimePicker
-        sx={{
-          maxWidth: "10vw",
-          minWidth: "5vw",
-        }}
-        label="hh:mm"
-        ampm={false}
-        value={selectedTime}
-        onChange={(newTime) => handleSetAlarm(newTime)}
-      />
+      {isMobile ? (
+        <IconButton onClick={() => setOpen(true)}>
+          <AlarmIcon />
+        </IconButton>
+      ) : (
+        <TimePicker
+          sx={{
+            maxWidth: "10vw",
+            minWidth: "8vw",
+          }}
+          label="hh:mm"
+          ampm={false}
+          value={selectedTime}
+          onChange={(newTime) => handleSetAlarm(newTime)}
+        />
+      )}
+
+      {/* TimePicker pro mobil */}
+      {isMobile && (
+        <TimePicker
+          sx={{ display: "none" }} // Skrytý, ale stále interaktivní
+          open={open}
+          onClose={() => setOpen(false)}
+          label="hh:mm"
+          ampm={false}
+          value={selectedTime}
+          onChange={(newTime) => handleSetAlarm(newTime)}
+        />
+      )}
     </LocalizationProvider>
   );
 };
